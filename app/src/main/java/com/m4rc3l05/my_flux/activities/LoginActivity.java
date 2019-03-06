a26ad1a9ee83251406e62991684023e6775e533e
João Baptista <joaobat@live.com.pt>
2019-03-06 21:26:41 +0000

    optimise imports
    Signed-off-by: João Baptista <joaobat@live.com.pt>

--- app/src/main/java/com/m4rc3l05/my_flux/activities/LoginActivity.java
+++ app/src/main/java/com/m4rc3l05/my_flux/activities/LoginActivity.java
@@ -4,15 +4,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.widget.AbsListView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
